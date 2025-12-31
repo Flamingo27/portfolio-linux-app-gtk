a185a58f656cfef47e24c5f757d38c4d4c3bfff4
Alokparna Mitra <185954983+Flamingo27@users.noreply.github.com>
2025-12-31 21:53:48 +0530

    Fix: Make symbolic icons theme-aware
    The SVG icon files for the hero page were previously hardcoded with 'fill="#ffffff"',
    causing them to appear black in both light and dark modes. This commit changes the
    'fill' attribute to 'currentColor' in all relevant SVG files, allowing them to
    properly inherit the foreground color from the GTK theme.
    
    Additionally, this commit includes minor adjustments to icon pixel sizes in
    'portfolio-hero-page.ui' and updates to
    'portfolio-linux-app-gtk-application.c' and 'portfolio-linux-app-gtk.gresource.xml'
    to ensure correct icon loading and application initialization.

--- src/portfolio-linux-app-gtk-application.c
+++ src/portfolio-linux-app-gtk-application.c
@@ -1,5 +1,6 @@
 #include "config.h"
 #include <glib/gi18n.h>
+#include <gtk/gtk.h>
 #include <adwaita.h>
 
 #include "portfolio-linux-app-gtk-application.h"
@@ -7,7 +8,7 @@
 
 struct _PortfolioLinuxAppGtkApplication
 {
-	AdwApplication parent_instance;
+    AdwApplication parent_instance;
 };
 
 G_DEFINE_FINAL_TYPE (PortfolioLinuxAppGtkApplication, portfolio_linux_app_gtk_application, ADW_TYPE_APPLICATION)
@@ -16,38 +17,53 @@ PortfolioLinuxAppGtkApplication *
 portfolio_linux_app_gtk_application_new (const char        *application_id,
                                          GApplicationFlags  flags)
 {
-	g_return_val_if_fail (application_id != NULL, NULL);
+    g_return_val_if_fail (application_id != NULL, NULL);
 
-	return g_object_new (PORTFOLIO_LINUX_APP_GTK_TYPE_APPLICATION,
-	                     "application-id", application_id,
-	                     "flags", flags,
-	                     "resource-base-path", "/com/alokparna/portfolio/gtk",
-	                     NULL);
+    return g_object_new (PORTFOLIO_LINUX_APP_GTK_TYPE_APPLICATION,
+                         "application-id", application_id,
+                         "flags", flags,
+                         "resource-base-path", "/org/alokparna/portfolio/gtk",
+                         NULL);
 }
 
 static void
 portfolio_linux_app_gtk_application_activate (GApplication *app)
 {
-	GtkWindow *window;
+    GtkWindow *window;
 
-	g_assert (PORTFOLIO_LINUX_APP_GTK_IS_APPLICATION (app));
+    g_assert (PORTFOLIO_LINUX_APP_GTK_IS_APPLICATION (app));
 
-	window = gtk_application_get_active_window (GTK_APPLICATION (app));
+    window = gtk_application_get_active_window (GTK_APPLICATION (app));
 
-	if (window == NULL)
-		window = g_object_new (PORTFOLIO_LINUX_APP_GTK_TYPE_WINDOW,
-		                       "application", app,
-		                       NULL);
+    if (window == NULL)
+        window = g_object_new (PORTFOLIO_LINUX_APP_GTK_TYPE_WINDOW,
+                               "application", app,
+                               NULL);
 
-	gtk_window_present (window);
+    gtk_window_present (window);
+}
+
+static void
+portfolio_linux_app_gtk_application_startup (GApplication *app)
+{
+    /* C90 Rule: Variables must be declared at the top */
+    GtkIconTheme *icon_theme;
+
+    /* Chain up to the parent startup first */
+    G_APPLICATION_CLASS (portfolio_linux_app_gtk_application_parent_class)->startup (app);
+
+    /* Now we can do the work */
+    icon_theme = gtk_icon_theme_get_for_display (gdk_display_get_default ());
+    gtk_icon_theme_add_resource_path (icon_theme, "/org/alokparna/portfolio/gtk/icons");
 }
 
 static void
 portfolio_linux_app_gtk_application_class_init (PortfolioLinuxAppGtkApplicationClass *klass)
 {
-	GApplicationClass *app_class = G_APPLICATION_CLASS (klass);
+    GApplicationClass *app_class = G_APPLICATION_CLASS (klass);
 
-	app_class->activate = portfolio_linux_app_gtk_application_activate;
+    app_class->activate = portfolio_linux_app_gtk_application_activate;
+    app_class->startup = portfolio_linux_app_gtk_application_startup;
 }
 
 static void
@@ -55,23 +71,23 @@ portfolio_linux_app_gtk_application_about_action (GSimpleAction *action,
                                                   GVariant      *parameter,
                                                   gpointer       user_data)
 {
-	static const char *developers[] = {"Alokparna Mitra", NULL};
-	PortfolioLinuxAppGtkApplication *self = user_data;
-	GtkWindow *window = NULL;
+    static const char *developers[] = {"Alokparna Mitra", NULL};
+    PortfolioLinuxAppGtkApplication *self = user_data;
+    GtkWindow *window = NULL;
 
-	g_assert (PORTFOLIO_LINUX_APP_GTK_IS_APPLICATION (self));
+    g_assert (PORTFOLIO_LINUX_APP_GTK_IS_APPLICATION (self));
 
-	window = gtk_application_get_active_window (GTK_APPLICATION (self));
+    window = gtk_application_get_active_window (GTK_APPLICATION (self));
 
-	adw_show_about_dialog (GTK_WIDGET (window),
-	                       "application-name", "Alokparna Portfolio",
-	                       "application-icon", "com.alokparna.portfolio.gtk",
-	                       "developer-name", "Alokparna Mitra",
-	                       "translator-credits", _("translator-credits"),
-	                       "version", "0.1.0",
-	                       "developers", developers,
-	                       "copyright", "© 2025 Alokparna Mitra",
-	                       NULL);
+    adw_show_about_dialog (GTK_WIDGET (window),
+                           "application-name", "Alokparna Portfolio",
+                           "application-icon", "org.alokparna.portfolio.gtk",
+                           "developer-name", "Alokparna Mitra",
+                           "translator-credits", _("translator-credits"),
+                           "version", "0.1.0",
+                           "developers", developers,
+                           "copyright", "© 2025 Alokparna Mitra",
+                           NULL);
 }
 
 static void
@@ -79,16 +95,14 @@ portfolio_linux_app_gtk_application_quit_action (GSimpleAction *action,
                                                  GVariant      *parameter,
                                                  gpointer       user_data)
 {
-	PortfolioLinuxAppGtkApplication *self = user_data;
-
-	g_assert (PORTFOLIO_LINUX_APP_GTK_IS_APPLICATION (self));
-
-	g_application_quit (G_APPLICATION (self));
+    PortfolioLinuxAppGtkApplication *self = user_data;
+    g_assert (PORTFOLIO_LINUX_APP_GTK_IS_APPLICATION (self));
+    g_application_quit (G_APPLICATION (self));
 }
 
 static const GActionEntry app_actions[] = {
-	{ "quit", portfolio_linux_app_gtk_application_quit_action },
-	{ "about", portfolio_linux_app_gtk_application_about_action },
+    { "quit", portfolio_linux_app_gtk_application_quit_action },
+    { "about", portfolio_linux_app_gtk_application_about_action },
 };
 
 static void
@@ -96,13 +110,13 @@ portfolio_linux_app_gtk_application_init (PortfolioLinuxAppGtkApplication *self)
 {
     AdwStyleManager *style_manager;
 
-	g_action_map_add_action_entries (G_ACTION_MAP (self),
-	                                 app_actions,
-	                                 G_N_ELEMENTS (app_actions),
-	                                 self);
-	gtk_application_set_accels_for_action (GTK_APPLICATION (self),
-	                                       "app.quit",
-	                                       (const char *[]) { "<control>q", NULL });
+    g_action_map_add_action_entries (G_ACTION_MAP (self),
+                                     app_actions,
+                                     G_N_ELEMENTS (app_actions),
+                                     self);
+    gtk_application_set_accels_for_action (GTK_APPLICATION (self),
+                                           "app.quit",
+                                           (const char *[]) { "<control>q", NULL });
 
     style_manager = adw_style_manager_get_default ();
     adw_style_manager_set_color_scheme (style_manager, ADW_COLOR_SCHEME_DEFAULT);
